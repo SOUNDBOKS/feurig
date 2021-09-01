09158c6a72c730563b07de977ed1d3f7983a06af
Mark-Oliver Junge <root@nyrox.dev>
2021-09-01 11:22:51 +0200

    Proper typings

--- feurig/src/index.ts
+++ feurig/src/index.ts
@@ -2,18 +2,23 @@ import { AsyncLocalStorage } from "async_hooks"
 import { performance } from "perf_hooks"
 
 const asyncStorage = new AsyncLocalStorage();
-    
-export const measure = async (name: string, cb: () => any) => {
+
+export interface TimingNode {
+    start: number
+    end: number
+    children: TimingNode[]
+}
+
+export async function measure<T>(name: string, cb: () => Promise<T>): Promise<[T, TimingNode]> {
     let node = { name, children: [], start: performance.now(), end: undefined }
     
-
-    await asyncStorage.run(node, cb)
+    let ret = await asyncStorage.run(node, cb)
     node.end = performance.now()
-
+    
     if (asyncStorage.getStore()) {
         (asyncStorage.getStore() as any).children.push(node)
-        return asyncStorage.getStore()
+        return [ret, asyncStorage.getStore() as TimingNode]
     } {
-        return node
+        return [ret, node]
     }
 }
